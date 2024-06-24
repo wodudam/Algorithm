0c45868a5a790d73b98fb5c69cc7180abfbbab95
wodudam <129144473+wodudam@users.noreply.github.com>
2024-06-25 00:20:57 +0900

    [Bronze V] Title: A+B - 3, Time: 248 ms, Memory: 17820 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int count = sc.nextInt();
+		
+		for (int i=0; i<count; i++) {
+			int num1 = sc.nextInt();
+			int num2 = sc.nextInt();
+			
+			System.out.println(num1 + num2);
+		}
+	}
+
+}
