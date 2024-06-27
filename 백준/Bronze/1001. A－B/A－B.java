1c43e84e5a6e87da0820015504c2c4219ab45b63
wodudam <129144473+wodudam@users.noreply.github.com>
2024-06-27 15:15:09 +0900

    [Bronze V] Title: A-B, Time: 128 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -1,12 +1,19 @@
-import java.util.*;
-public class Main{
-	public static void main(String args[]){
-		int a, b;
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(str, " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        bw.write(String.valueOf(a - b));
+        bw.close();
         
-        Scanner sc = new Scanner(System.in);
-        
-		a = sc.nextInt();
-		b = sc.nextInt();
-		System.out.println(a - b);
-	}
+    }
 }
