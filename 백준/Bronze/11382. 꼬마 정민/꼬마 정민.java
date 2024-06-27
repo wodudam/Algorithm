c5119af10282bdd230ae57912351e9cc9a7d00b0
wodudam <129144473+wodudam@users.noreply.github.com>
2024-06-27 23:52:52 +0900

    [Bronze V] Title: 꼬마 정민, Time: 132 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -1,13 +1,18 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-class Main{
-	public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        
-		long a = sc.nextLong();
-		long b = sc.nextLong();
-        long c = sc.nextLong();
-        
-		System.out.println(a + b + c);
-	}
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        String[] splits = str.split(" ");
+
+        long a = Long.parseLong(splits[0]);
+        long b = Long.parseLong(splits[1]);
+        long c = Long.parseLong(splits[2]);
+
+        System.out.println(a + b + c);
+    }
 }
