0f7fd6822a09efadc67fba98b6fecb1781bb9b4a
wodudam <129144473+wodudam@users.noreply.github.com>
2024-06-28 15:41:23 +0900

    [Bronze III] Title: 알람 시계, Time: 132 ms, Memory: 16092 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -1,24 +1,31 @@
-import java.util.*;
-public class Main{
-	public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] splits = br.readLine().split(" ");
+
+        int h = Integer.parseInt(splits[0]);
+        int m = Integer.parseInt(splits[1]);
+
+//        if (m > 45)
+//            System.out.println(h + " " + (m - 45));
+//        else {
+//            if (h > 0)
+//                System.out.println((h - 1) + " " + (m + 15));
+//            else
+//                System.out.println((h + 23) + " " + (m + 15));
+//        }
         
-		int h = sc.nextInt();
-		int m = sc.nextInt();
-        
-        if (m<45) 
-        {
-            h--;
-            m = m+15;
-            if (h<0) 
-            {
-                h = 23;
-            }
-            System.out.println(h + " " + m);
-        }
-        else if (m >=45)
-        {
-            System.out.println(h + " " + (m-45));
-        }
-	}
+        if (m < 45)
+            if (h == 0)
+                System.out.println((h + 23) + " " + (m + 15));
+            else
+                System.out.println((h - 1) + " " + (m + 15));
+        else
+            System.out.println(h + " " + (m - 45));
+    }
 }
