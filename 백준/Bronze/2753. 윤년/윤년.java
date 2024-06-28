01c30e4819131ec008f471eb84675c2105ad7f51
wodudam <129144473+wodudam@users.noreply.github.com>
2024-06-28 14:53:12 +0900

    [Bronze V] Title: 윤년, Time: 124 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,16 +1,19 @@
-import java.util.*;
-public class Main{
-	public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        int a = Integer.parseInt(s);
+
+        //System.out.println(((a % 4 == 0) && (a % 100 != 0)) || (a %400 ==0) ? "1" : "0");
         
-		int a = sc.nextInt();
-        
-        if ((a%4 == 0) && (a%100 != 0))  
-            System.out.println("1");
-        else if (a%400 == 0)
+        if (((a % 4 == 0) && (a % 100 != 0)) || (a %400 ==0))
             System.out.println("1");
         else
             System.out.println("0");
-
-	}
+    }
 }
