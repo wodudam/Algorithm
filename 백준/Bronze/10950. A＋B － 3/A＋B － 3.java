dbe53847a8852cb5a256ff0b18aab4a513069b50
wodudam <129144473+wodudam@users.noreply.github.com>
2024-06-30 02:06:15 +0900

    [Bronze V] Title: A+B - 3, Time: 108 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -1,18 +1,22 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int count = sc.nextInt();
-		
-		for (int i=0; i<count; i++) {
-			int num1 = sc.nextInt();
-			int num2 = sc.nextInt();
-			
-			System.out.println(num1 + num2);
-		}
-	}
+        int count = Integer.parseInt(br.readLine());
 
+        for (int i = 0; i < count; i++) {
+            String[] splits = br.readLine().split(" ");
+            int a = Integer.parseInt(splits[0]);
+            int b = Integer.parseInt(splits[1]);
+
+            int result = a + b;
+           // bw.write(result);
+            System.out.println(result);
+        }
+       // bw.flush();
+       // bw.close();
+    }
 }
