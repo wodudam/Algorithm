7d47858d38bd55d5ba6277ad63c7b8e009e7f7c3
wodudam <129144473+wodudam@users.noreply.github.com>
2024-06-29 23:11:55 +0900

    [Bronze V] Title: 구구단, Time: 140 ms, Memory: 16160 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,15 +1,14 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int i = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int dan = sc.nextInt();
-		
-		for (int i=1; i<=9; i++) {
-			System.out.println(dan + " * " + i + " = " + dan*i);
-		}
-	}
-
+            for (int j = 1; j <= 9; j++) {
+                System.out.println(i + " * " + j + " = " + i * j);
+        }
+    }
 }
