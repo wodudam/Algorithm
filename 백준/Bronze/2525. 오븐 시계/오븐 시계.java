cd1737dbbe0b78a2b5c902a8fe0327bd0c59b28e
wodudam <129144473+wodudam@users.noreply.github.com>
2024-06-29 22:18:49 +0900

    [Bronze III] Title: 오븐 시계, Time: 124 ms, Memory: 15944 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -1,19 +1,23 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		        Scanner sc = new Scanner(System.in);
-		        
-				int a = sc.nextInt();
-				int b = sc.nextInt();
-		        int c = sc.nextInt();
-		         
-		        int result = a*60 + b + c;
-		        int h = (result/60)%24;
-		        int m = result%60;
-		       
-		        System.out.println(h + " " + m);        
-	}
+        String[] splits = br.readLine().split(" ");
+
+        int h = Integer.parseInt(splits[0]);
+        int m = Integer.parseInt(splits[1]);
+        int t = Integer.parseInt(br.readLine());
+
+        if ((m + t) < 60)
+            System.out.println(h + " " + (m + t));
+        else
+            if (h + ((m + t) / 60) >= 24)
+                System.out.println((h + ((m + t) / 60) - 24) + " " + ((m + t) % 60));
+            else
+                System.out.println((h + ((m + t) / 60)) + " " + ((m + t) % 60));
+    }
 }
