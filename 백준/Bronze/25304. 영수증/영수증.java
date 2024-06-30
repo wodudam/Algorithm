33ceee6ef46b284edb42bdd7503aa866a513d9df
wodudam <129144473+wodudam@users.noreply.github.com>
2024-06-30 14:27:00 +0900

    [Bronze IV] Title: 영수증, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -1,24 +1,23 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int X = sc.nextInt();
-		int N = sc.nextInt();
-		int sum = 0;
-		
-		for (int i=0; i<N; i++)	{
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			sum = sum + a*b;
-		}
-		if (X == sum)
-			System.out.println("Yes");
-		else
-			System.out.println("No");
-		
-	}
+        int totalPrice = Integer.parseInt(br.readLine());
+        int count = Integer.parseInt(br.readLine());
+        int price = 0;
 
+        for (int i = 0; i < count; i++) {
+            String[] splits = br.readLine().split(" ");
+            int a = Integer.parseInt(splits[0]);
+            int b = Integer.parseInt(splits[1]);
+            price = price + (a * b);
+        }
+
+        if (totalPrice == price)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
 }
