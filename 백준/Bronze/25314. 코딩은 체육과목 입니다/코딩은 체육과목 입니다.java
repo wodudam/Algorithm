a609d663d382576a53d63d09e65ab954aa4e7df6
wodudam <129144473+wodudam@users.noreply.github.com>
2024-07-02 16:22:22 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 128 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -1,16 +1,14 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int N = sc.nextInt();
-		
-		for (int i=0; i<N/4; i++) {
-			System.out.print("long ");
-		}
-		System.out.println("int");
-	}
+        int n = Integer.parseInt(br.readLine()) / 4;
 
+        for (int i = 0; i < n; i++) {
+            System.out.print("long ");
+        }
+        System.out.print("int");
+    }
 }
