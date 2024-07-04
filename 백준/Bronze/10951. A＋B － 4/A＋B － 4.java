920618ce47d4be9b830991d21b6ea6c0f14aeb0d
wodudam <129144473+wodudam@users.noreply.github.com>
2024-07-04 16:43:23 +0900

    [Bronze V] Title: A+B - 4, Time: 148 ms, Memory: 15856 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter((new OutputStreamWriter(System.out)));
+        String str;
+
+        while ((str = br.readLine()) != null) {
+            String[] splits = str.split(" ");
+            int a = Integer.parseInt(splits[0]);
+            int b = Integer.parseInt(splits[1]);
+            int sum = a + b;
+
+            bw.write(sum + "\n");
+        }
+        bw.close();
+    }
+}
