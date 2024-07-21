8ba4279facf027c1982da3273428ced00f168d91
wodudam <129144473+wodudam@users.noreply.github.com>
2024-07-21 21:53:15 +0900

    [Bronze V] Title: 아스키 코드, Time: 128 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String s = br.readLine();
+        int s = br.read();
 
-        System.out.println((int)s.charAt(0));
+        System.out.println(s);
     }
 }
 
