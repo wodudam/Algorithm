23e56851f448586be62c65cd430977783e4757a1
wodudam <129144473+wodudam@users.noreply.github.com>
2024-07-24 01:22:46 +0900

    [Bronze II] Title: 단어의 개수, Time: 240 ms, Memory: 18444 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -4,15 +4,11 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        bw.write(String.valueOf(st.countTokens()));
-        bw.flush();
+        System.out.println(st.countTokens());
 
         br.close();
-        bw.close();
-
     }
 }
 
