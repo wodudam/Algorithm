5701301609bf35b63219b0eb90d68c3f3170c619
wodudam <129144473+wodudam@users.noreply.github.com>
2024-10-12 01:30:10 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 116 ms, Memory: 15904 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -1,18 +1,21 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int chess[] = {1,1,2,2,2,8};
-        int input[] = new int[6];
+        int[] chess = {1, 1, 2, 2, 2, 8};
 
-        for(int i =0;i<chess.length;i++) {
-            input[i] = sc.nextInt();
+        String[] s = br.readLine().split(" ");
+
+        for (int i = 0; i < s.length; i++) {
+            bw.write(chess[i] - Integer.parseInt(s[i]) + " ");
+            bw.flush();
         }
-        for(int i =0;i<chess.length;i++) {
-            System.out.print(chess[i]-input[i]+" ");
-        }
+
+        br.close();
+        bw.close();
     }
 }
