5541da9f30465a1302d237489840f6b1a11b3b47
wodudam <129144473+wodudam@users.noreply.github.com>
2025-01-15 16:59:07 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 184 ms, Memory: 17564 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,33 +1,41 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		int a[][] = new int[n][m];
-		int b[][] = new int[n][m];
-		
-		for (int i=0; i<n; i++) {
-			for (int j=0; j<m; j++) {
-				a[i][j] = sc.nextInt();
-			}
-		}
-		
-		for (int i=0; i<n; i++) {
-			for (int j=0; j<m; j++) {
-				b[i][j] = sc.nextInt();
-			}
-		}
-		
-		for (int i=0; i<n; i++) {
-			for (int j=0; j<m; j++) {
-				System.out.print(a[i][j] + b[i][j] + " ");
-				if(j==m-1)
-					System.out.println();
-			}
-		}
-	}
+        String[] Input = br.readLine().split(" ");
+        int n = Integer.parseInt(Input[0]);
+        int m = Integer.parseInt(Input[1]);
 
+        int a[][] = new int[n][m];
+        int b[][] = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            String[] line = br.readLine().split(" ");
+            for (int j = 0; j < m; j++) {
+                a[i][j] = Integer.parseInt(line[j]);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            String[] line = br.readLine().split(" ");
+            for (int j = 0; j < m; j++) {
+                b[i][j] = Integer.parseInt(line[j]);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                bw.write(a[i][j] + b[i][j] + " ");
+            }
+            bw.newLine();
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
 }
+
